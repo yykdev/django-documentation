84abc4b40a01fe3dd309ea83ddb826ffd47cb365
yykdev <assavictory@gmail.com>
2017-06-05 16:20:45 +0900

    Pizza의 __str__에 자신의 toppings목록을 리턴

--- django_app/introduction_to_models/models/topping_and_pizza.py
+++ django_app/introduction_to_models/models/topping_and_pizza.py
@@ -16,7 +16,25 @@ class Pizza(models.Model):
     toppings = models.ManyToManyField(Topping)
 
     def __str__(self):
-        return self.name
+        # 자신이 가지고 있는 토핑목록을 뒤에 출력
+        # ex) 치즈피자(치즈 토마토)
+        #
+        # for문 방식. 파이썬 스럽지 않다.
+        # topping_string = ''
+        # for topping in self.toppings.all():
+        #     topping_string += topping.name
+        #     topping_string += ', '
+        #
+        # # 마지막의 두번째 글자를 잘라낸다 ( slice 연산 )
+        # # [:-2] 처음 부터 뒤에서 두번째 이전 까지
+        # topping_string = topping_string[:-2]
+
+
+        # join, 리스트 컴프리헨션을 사용하여 한줄로 작성 한다.
+        return '{} ( {} )'.format(
+            self.name,
+            ','.join([ topping.name for topping in self.toppings.all() ])
+        )
 
     class Meta:
         ordering = ('name',)
