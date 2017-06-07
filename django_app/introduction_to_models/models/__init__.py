2febe4eb1d2f546e58e87f4f52b50e007712a5d2
yykdev <assavictory@gmail.com>
2017-06-07 15:39:30 +0900

    abstract class

--- django_app/introduction_to_models/models/__init__.py
+++ django_app/introduction_to_models/models/__init__.py
@@ -4,3 +4,4 @@ from .many_to_one_example import Reporter, Article
 from .topping_and_pizza import Topping, Pizza
 from .intermediate import Player, Club, TradeInfo
 from .abstract_base_classes import CommonInfo, Student, Teacher
+from .multi_table_inheritance import CommonInfo2, Student2, Teacher2
--- django_app/introduction_to_models/models/abstract_base_classes.py
+++ django_app/introduction_to_models/models/abstract_base_classes.py
@@ -1,12 +1,26 @@
 from django.db import models
+#from .car import Car
 
 
 class CommonInfo(models.Model):
     name = models.CharField(max_length=100)
     age = models.PositiveIntegerField()
+    # cars = models.ManyToManyField(
+    #     Car,
+    #     related_name='%(app_label)s_%(class)ss',
+    #     related_query_name='%(app_label)s_%(class)s',
+    #     # app_label : 모듈 이름 introduction_to_models
+    #     # class : 호출 할 클래스(데이터 모델 클래스) 명의 소문자, student, teacher
+    # )
+
 
     class Meta:
         abstract = True
+        # Meta 옵션 : abstract - 클래스의 추상화.
+        # 일반 모델로 사용 할 수 없으며, DB로 생성 할 수 없다.
+        #
+        # 모델 클래스가 상속받아 모델을 생성할 경우 추상 클래스의 속성을 사용할 수 있다.
+        # 모델 DB생성시 공통된 부분을 반복 생성 하지 않기 위한 방법.
 
 
 class Student(CommonInfo):
@@ -21,6 +35,7 @@ class Student(CommonInfo):
 
     class Meta:
         db_table = 'introduction_to_models_abc_student'
+        # Meta 옵션 : db_table - 테이블 명을 지정 할 수 있음
 
 
 class Teacher(CommonInfo):
